451c3dc22188afb6fb4b513f1cb51030687ea213
Francisco <francisco.marchini@uranet.com.br>
2020-09-16 14:25:14 -0300

    atualizando

--- src/components/templates/Contatos.vue
+++ src/components/templates/Contatos.vue
@@ -264,9 +264,13 @@ export default {
         break;
         case "aguardando":
           this.abaAberta = "atendimento"
+          this.totalMsgNovas = ''
+          this.totalClientesNovos = ''
         break;
         default:
           this.abaAberta = "atendimento"
+          this.totalMsgNovas = ''
+          this.totalClientesNovos = ''
         break;
       }
     },
