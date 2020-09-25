ecdb49d036d2f9c96650318fa9f27095aabf1c2f
Augusto <augusto.seabra@uranet.com.br>
2020-09-25 11:46:55 -0300

    subindo contador da agenda

--- src/components/templates/Contatos.vue
+++ src/components/templates/Contatos.vue
@@ -114,7 +114,10 @@
                 <p v-if="fechado">{{ formataSigla(atd.nome_usu[1], 'lower') }}</p>
               </div>
               <template v-if="!fechado">{{ atd.nome_usu }}</template>
-              <span class="data-retorno">{{ formataData(atd.data) }}</span>
+              <div class="retorno-container" :id="'retorno_'+indice">
+                <span class="data-retorno">{{ formataData(atd.data) }}</span>
+                <span class="contador-data-retorno"><i class="fas fa-stopwatch"></i>{{ formataData(atd.data, "retorno", `#retorno_${indice}`) }}</span>
+              </div>
             </li>
           </ul>
         </div>
@@ -157,7 +160,8 @@ export default {
       abaAberta: 'atendimento',
       totalMsgNovas: '',
       totalClientesNovos: '',
-      contadorErros: 0
+      contadorErros: 0,
+      contadorErrosAgenda: 0
     };
   },
   watch: {
@@ -292,20 +296,98 @@ export default {
     adicionaCaso(caso){
       this.$store.dispatch('setCaso', caso)
     },
-    formataData(data){
+    formataData(data, retorno, id){
       let aux = data.split(/\s/)
       let dataAux = aux[0]
       let horaAux = aux[1]
 
-      dataAux = dataAux.replace(/-/g, "\/")
-      dataAux = dataAux.split(/\//)
-      dataAux = dataAux.reverse()
-      dataAux = dataAux.join("/")
+      if(!retorno){
+        dataAux = dataAux.replace(/-/g, "\/")
+        dataAux = dataAux.split(/\//)
+        dataAux = dataAux.reverse()
+        dataAux = dataAux.join("/")
+  
+        horaAux = horaAux.replace(/:/g, "h")
+        horaAux = horaAux.slice(0, horaAux.length-3)
+  
+        return dataAux + ' ' + horaAux
+      }else{
+        const spanContador = document.querySelector(`${id} span.contador-data-retorno`)
+        if(!spanContador){
+          this.contadorErrosAgenda++
+          if(this.contadorErrosAgenda < 10){
+            setTimeout(() => {
+              this.formataData(data, retorno, id)
+            }, 500)
+          }
+          return
+        }
 
-      horaAux = horaAux.replace(/:/g, "h")
-      horaAux = horaAux.slice(0, horaAux.length-3)
+        // Diferenca entre dias
+        let dataAtual = new Date()
+        let dataAgenda = new Date(data)
+        
+        let difDatas = Date.UTC(dataAgenda.getYear(), dataAgenda.getMonth(), dataAgenda.getDate(),0,0,0) - Date.UTC(dataAtual.getYear(), dataAtual.getMonth(), dataAtual.getDate(),0,0,0)
+        // Retorna o valor em horas
+        difDatas = difDatas / 1000 / 60 / 60
+        // Retorna o valor em dias
+        difDatas = difDatas / 24
 
-      return dataAux + ' ' + horaAux
+        // Mesmo dia
+        if(difDatas == 0){
+          // Diferenca entre horarios
+          let difHoras = dataAgenda.getHours() - dataAtual.getHours()
+          let difMinutos = dataAgenda.getMinutes() - dataAtual.getMinutes()
+          
+          // Significa que os horarios estao em horas diferentes (ex: 11:00:00 e 10:59:59)
+          if(difHoras > 0){
+            // Significa que os minutos do horario atual sao maiores do que os minutos do horario agendado
+            if(difMinutos < 0){
+              difMinutos = 60 - Math.abs(difMinutos)
+              difHoras = difHoras - 1
+            }
+            if(difHoras == 0){
+              return `${difMinutos}min`
+            }else{
+              return `${difHoras}h e ${difMinutos}min`
+            }
+          }else{
+            // Significa que chegou a hora de chamar o ctt
+            if(difHoras == 0 && difMinutos == 0){
+              spanContador.classList.add("d-none")
+              return 
+            }
+
+            // Significa que o horario agendado ja passou
+            if(difHoras <= 0 && difMinutos < 0){
+              spanContador.classList.add("d-none")
+              return
+            }
+
+            // Significa que o horario ainda nao chegou
+            spanContador.classList.remove("d-none")
+            return `${difMinutos}min`
+          }
+        }else{
+          // Dias diferentes
+          // Data atual maior que data agendada
+          if(difDatas < 0){
+            spanContador.classList.add("d-none")
+            return 
+          }else{
+          // Data agendado maior que data atual
+            spanContador.classList.remove("d-none")
+
+            if(difDatas == 1){
+              return `${difDatas} dia`
+            }else{
+              return `${difDatas} dias`
+            }
+          }
+        }
+
+      }
+
     },
     statusMensagens(atd){
 
