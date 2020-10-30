0cbd060856b9962b0c23130415e8f199c3e04c95
Augusto <augusto.seabra@uranet.com.br>
2020-10-30 16:09:05 -0300

    ajustando

--- src/components/Mensagens.vue
+++ src/components/Mensagens.vue
@@ -3,7 +3,7 @@
     <div class="mensagem">
       <div v-if="anexo" class="mensagem-div-anexo">
         <img v-if="imgAnexo" :src="imgAnexo" alt="Imagem Anexada">
-        <a v-if="imgAnexo" :href="imgAnexo" download> Baixar Imagem <i class="fas fa-download"></i> </a>
+        <a v-if="imgAnexo" :href="imgAnexo" download target="_blank"> Baixar Imagem <i class="fas fa-download"></i> </a>
         <div v-if="tipoDoc" class="anexo-container">
           <a :href="docAnexo" target="_blank" :title="`${tipoDoc} - ${nomeArquivo}`">
             <i class="fas fa-file-alt"></i>
